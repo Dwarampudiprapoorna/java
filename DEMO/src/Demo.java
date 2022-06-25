0a946b3c3046352d7bbdc7cbb0849eece127da8a
acer <acer@TMP249-G2-M.bbrouter>
2022-06-25 13:27:56 +0530

    printtive

---
+++ DEMO/src/Demo.java
@@ -0,0 +1,10 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int pincode = 500085;
+System.out.println(pincode);
+	}
+
+}
---
+++ DEMO/src/operator.java
@@ -0,0 +1,27 @@
+
+public class operator {
+public static void main(String[]args)
+{
+	
+	int a=9;
+	int b=5;
+	System.out.println(a-b);
+	System.out.println(a*b);
+	{
+		int z=a++;
+		System.out.println("the output is z:"+z);
+		int x=--a;
+		System.out.println("the value is x:"+x);
+		int y=++a; 
+		System.out.println("the no is y:"+y);
+		int p=a&b;
+		System.out.println("the value is:"+p);
+		int s=a/b;
+		System.out.println("the value is:"+s);
+	
+		
+		
+		
+	}
+}
+}
