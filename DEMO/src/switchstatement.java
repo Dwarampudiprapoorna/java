af686b493770ff732ab4b39eb765747de2995b09
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 14:39:44 +0530

    switch statement

---
+++ DEMO/src/switchstatement.java
@@ -0,0 +1,30 @@
+
+public class switchstatement {
+
+	public static void main(String[] args) {
+		int a = 3;
+		switch(a)
+		{
+		case 1:
+			System.out.println("ur in case 1");
+			break;
+		case 2:
+			System.out.println("ur in case 2");
+		case 3:
+			System.out.println("ur in case 3");
+		case 4:
+			System.out.println("ur in case 4");
+			break;
+			default:
+				System.out.println("no such case is variable");
+		}
+	}
+}
+		
+		
+	
+		
+
+	
+
+
