ba350ee03928ccf8e87f18e0e592d2dc6e2a336d
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 14:23:50 +0530

    Data types

---
+++ DEMO/src/Datatypes.java
@@ -0,0 +1,37 @@
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		//integer data type 
+		int a=6;
+		int b=4;
+		int c=a+b;    
+		System.out.println("enter the value c:" +c);
+		//char data type
+	 
+		
+		char letter ='M';
+		System.out.println(letter);
+		//float data type
+		float  n =42f%5f;
+		System.out.println(n);
+		//Double data type
+		double p =16d%4d;
+		System.out.println(p);
+		//Boolean data type
+		boolean var = true;
+		char var1 = 112;
+		System.out.println("enter the value var1:" +var1);
+		//string data type
+		String var2 = new String("prapoorna");
+		System.out.println(""+var2);
+		
+		
+
+	}
+}
+	
+
+	
+
+
